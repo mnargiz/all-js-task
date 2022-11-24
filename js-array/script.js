34cbdac3bd04e48251392c3ed39dc144ac756358
Nargiz <lmnargizl@gmail.com>
2022-11-25 03:25:29 +0400

    task

--- js-array/script.js
+++ js-array/script.js
@@ -1,32 +1,14 @@
 // 1. Find the minimum element of this array
 
-// first method
 // let array = [24, 90, 2001, 789, 8, -9];
 // console.log(Math.min(...array));
 
 
-// second method
-// let array = [24, 90, 2001, 789, 8, -9];
-// let minElement = array.reduce((a, b) => {
-//     return Math.min(a, b);
-// });
-// console.log(minElement);
-
- 
-
 
 // 2. Find the maximum element of this array
-// first method
+
 // let array = [24, 90, 2001, 789, 8, -9];
 // console.log(Math.max(...array));
-
-
-// second method
-// let array = [24, 90, 2001, 789, 8, -9];
-// let maxElement = array.reduce((a, b) => {
-//     return Math.max(a, b);
-// });
-// console.log(maxElement);
 
 
 
@@ -55,18 +37,91 @@
 //     let even = 0;
 //     let odd = 0;
 //     for (let i = 0; i < n; i++) {
-
-//         if (i % 2 == 0)
+//         if (i % 2 == 0) {
 //             even += array[i];
-//         else
+//         }
+//         else {
 //             odd += array[i];
+//         }
+
 //     }
 //     console.log(odd);
-//}
+// }
 
 
 
 //6. Reverse this array (array in reverse direction) - you have to create YOUR OWN function, which WILL NOT create any new array
-let array = [24, 90, 2001, 789, 8, -9];
+
+// let array = [24, 90, 2001, 789, 8, -9];
+// let reversed = array.reverse();
+// console.log(reversed);
 
 
+
+
+//7. Count the number of odd elements in the array
+
+// let array = [24, 90, 2001, 789, 8, -9];
+// const odds = array.filter(number => {
+//     return number % 2 == 1 || number % 2 == -1;
+// });
+
+// console.log(odds);
+
+
+
+
+//8. Swap the first and second half of the array, for example, for an array of 1234, the result is 3412, or for 12345 the result will be 45312.
+//I can't write
+
+
+
+
+//9. Sort the array in ascending order in one of the following ways: bubble sort, selection sort or insertion sort.
+
+// let array = [24, 90, 2001, 789, 8, -9];
+// function selectionSort(array) {
+
+//     for (let i = 0; i < array.length; i++) {
+//         let min = i;
+//         for (let j = i + 1; j < array.length; j++) {
+//             if (array[j] < array[min]) {
+//                 min = j;
+//             }
+//         }
+//         if (min != i) {
+//             let end = array[i];
+//             array[i] = array[min];
+//             array[min] = end;
+//         }
+//     }
+//     return array;
+// }
+// selectionSort(array);
+// console.log(array);
+
+
+
+
+//10. Sort the array in descending order in one of the ways (different from the method taken in the 9th task): bubble sort, selection sort or insertion sort.
+
+// let array = [24, 90, 2001, 789, 8, -9];
+// function selectionSort(array) {
+
+//     for (let i = 0; i < array.length; i++) {
+//         let max = i;
+//         for (let j = i + 1; j < array.length; j++) {
+//             if (array[j] > array[max]) {
+//                 max = j;
+//             }
+//         }
+//         if (max != i) {
+//             let end = array[i];
+//             array[i] = array[max];
+//             array[max] = end;
+//         }
+//     }
+//     return array;
+// }
+// selectionSort(array);
+// console.log(array);
