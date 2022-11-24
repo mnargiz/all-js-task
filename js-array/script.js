4a17872608507bb1b24e2de49b1c067f1a434572
Nargiz <lmnargizl@gmail.com>
2022-11-24 12:35:12 +0400

    ftggfghfv

---
+++ js-array/script.js
@@ -0,0 +1,72 @@
+// 1. Find the minimum element of this array
+
+// first method
+// let array = [24, 90, 2001, 789, 8, -9];
+// console.log(Math.min(...array));
+
+
+// second method
+// let array = [24, 90, 2001, 789, 8, -9];
+// let minElement = array.reduce((a, b) => {
+//     return Math.min(a, b);
+// });
+// console.log(minElement);
+
+ 
+
+
+// 2. Find the maximum element of this array
+// first method
+// let array = [24, 90, 2001, 789, 8, -9];
+// console.log(Math.max(...array));
+
+
+// second method
+// let array = [24, 90, 2001, 789, 8, -9];
+// let maxElement = array.reduce((a, b) => {
+//     return Math.max(a, b);
+// });
+// console.log(maxElement);
+
+
+
+// 3. Find the index of the minimum element of this array
+
+// let array = [24, 90, 2001, 789, 8, -9];
+// console.log(array.indexOf(Math.min(...array)));
+
+
+
+
+//4. Find the index of the maximum element of this array
+
+// let array = [24, 90, 2001, 789, 8, -9];
+// console.log(array.indexOf(Math.max(...array)));
+
+
+
+
+//5. Calculate the sum of array's elements with odd indices
+
+// let array = [24, 90, 2001, 789, 8, -9];
+// let n = array.length;
+// OddSum(array, n);
+// function OddSum(arr, n) {
+//     let even = 0;
+//     let odd = 0;
+//     for (let i = 0; i < n; i++) {
+
+//         if (i % 2 == 0)
+//             even += array[i];
+//         else
+//             odd += array[i];
+//     }
+//     console.log(odd);
+//}
+
+
+
+//6. Reverse this array (array in reverse direction) - you have to create YOUR OWN function, which WILL NOT create any new array
+let array = [24, 90, 2001, 789, 8, -9];
+
+
